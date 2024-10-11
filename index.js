e120700cf55c68e8a70c7b133c2e64dde23bf48f
Travis Kaufman <travis.kaufman@gmail.com>
2024-10-10 20:14:21 -0400

    Fix some layout stuff

--- index.js
+++ index.js
@@ -3,16 +3,16 @@ import spec from "./viz/map.vg.json";
 
 async function main() {
   const res = await vegaEmbed("#vis", spec);
-  // const element = document.querySelector("#vis");
-  // const resizeObserver = new ResizeObserver(async ([entry]) => {
-  //   const { width, height } = entry.contentRect;
-  //   await res.view
-  //     .width(width - 8)
-  //     .height(height - )
-  //     .runAsync();
-  // });
+  const element = document.querySelector("#vis");
+  const resizeObserver = new ResizeObserver(async ([entry]) => {
+    const { width, height } = entry.contentRect;
+    await res.view
+      .width(width - 8)
+      .height(height - 8)
+      .runAsync();
+  });
 
-  // resizeObserver.observe(element);
+  resizeObserver.observe(element);
 }
 
 main().catch(console.error.bind(console));
