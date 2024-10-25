999acbcd4c96e60cd79ad70c88e4ea684fc841bd
Travis Kaufman <travis.kaufman@gmail.com>
2024-10-24 21:03:05 -0400

    Revert "New data prep + map fixup"
    This reverts commit f0e3f279d3d0cdd7ac0e5656b448f9aac2cef042.

--- scripts/prep-data.sql
+++ scripts/prep-data.sql
@@ -2,68 +2,109 @@ WITH
 ss AS (
     SELECT
         "Complex ID",
+        "GTFS Latitude",
+        "GTFS Longitude",
         Borough,
-        AVG("GTFS Latitude") AS complex_latitude,
-        AVG("GTFS Longitude") AS complex_longitude,
-        ARRAY_DISTINCT(ARRAY_AGG("Stop Name")) AS stop_names,
-        ARRAY_DISTINCT(FLATTEN(ARRAY_AGG(str_split_regex("Daytime Routes", ' ')))) AS lines
+        "Stop Name",
+        list_reduce(
+            list_sort(
+                -- The art pieces table does not have "W" so we need to remove
+                list_filter(
+                    str_split_regex("Daytime Routes", ' '),
+                    s -> s != 'W'
+                )
+            ),
+            (s1, s2) -> concat(trim(s1), ' ', trim(s2))
+        ) AS station_line,
     FROM
         main.subway_stations
-    GROUP BY
-        1, 2
 )
 , art AS (
     SELECT
         "Station Name",
-        str_split_regex(Line, ',') AS lines,
-        "Art Title"
+        "Art Date",
+        list_reduce(
+        list_sort(
+            str_split_regex(Line,
+        ',')
+        ),
+        (s1,
+        s2) -> concat(trim(s1),
+        ' ',
+        trim(s2))
+    ) AS art_line,
+        COUNT("Art Title") AS num_art_pieces
     FROM
         main.permanent_art_catalog pac
     WHERE
         Agency = 'NYCT'
+    GROUP BY
+        1,
+        2,
+        3
 )
-, art_complexes AS (
+, data AS (
     SELECT
         ss.*,
-        art."Art Title"
+        art."Art Date",
+        art.art_line,
+        COALESCE(art.num_art_pieces, 0) AS num_art_pieces
     FROM ss
     LEFT JOIN
-        art ON (ARRAY_CONTAINS(ss.stop_names, art."Station Name") AND ARRAY_HAS_ANY(ss.lines, art.lines))
+        art ON (ss."Stop Name" = art."Station Name" AND CONTAINS(art.art_line, ss.station_line))
 )
-, art_2024 AS (
+, art_data AS (
     SELECT
-        "Complex ID" AS complex_id,
-        ARBITRARY("Borough") AS borough,
-        ARBITRARY(complex_latitude) AS complex_latitude,
-        ARBITRARY(complex_longitude) AS complex_longitude,
-        COUNT(DISTINCT "Art Title") AS num_art_pieces
+        data."Complex ID" AS complex_id,
+        strptime(CAST(data."Art Date" AS text), '%Y') AS year_first_displayed,
+        data."Stop Name" AS stop_name,
+        data.Borough AS borough,
+        data.station_line AS line,
+        arbitrary(data."GTFS Latitude") AS latitude,
+        arbitrary(data."GTFS Longitude") AS longitude,
+        SUM(data.num_art_pieces) AS num_art_pieces
     FROM
-        art_complexes
+        data
     GROUP BY
-        1
+        1, 2, 3, 4, 5
 )
-, ridership_2024 AS (
+, ridership_data AS (
     SELECT
+        "date",
         station_complex_id,
         ARBITRARY(station_complex) AS station_complex,
         ARBITRARY(borough) AS borough,
-        SUM(ridership) AS ridership
+        SUM(ridership) AS ridership,
+        SUM(transfers) AS transfers
     FROM
         main.ridership_monthly rm
-    WHERE
-        YEAR("date") = 2024
     GROUP BY
-        1
+        1, 2
 )
 SELECT
-    a.complex_id,
-    r.station_complex,
-    r.borough,
-    a.complex_latitude,
-    a.complex_longitude,
-    r.ridership,
-    a.num_art_pieces
+    ridership_data."date" AS "date",
+    ridership_data.station_complex_id AS complex_id,
+    ridership_data.station_complex AS complex_name,
+    art_data.stop_name AS stop_name,
+    art_data.line AS line,
+    ridership_data.borough AS borough,
+    ARBITRARY(art_data.latitude) AS latitude,
+    ARBITRARY(art_data.longitude) AS longitude,
+    SUM(art_data.num_art_pieces) AS num_art_pieces,
+    ARBITRARY(ridership_data.ridership) AS ridership
 FROM
-    art_2024 a
+    ridership_data
 JOIN
-    ridership_2024 r ON (CAST(a.complex_id AS VARCHAR) = r.station_complex_id)
+    art_data ON (
+        ridership_data.station_complex_id = CAST(art_data.complex_id AS text)
+        AND (art_data.year_first_displayed IS NULL OR ridership_data."date" >= art_data.year_first_displayed)
+    )
+GROUP BY
+    1,2,3,4,5,6
+ORDER BY
+  "date" DESC,
+  num_art_pieces DESC,
+  ridership DESC,
+  stop_name ASC,
+  line ASC,
+  borough ASC
